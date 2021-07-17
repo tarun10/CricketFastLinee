d9a4281317c975ccb03aeb9240c155548a9bedd6
tarun bhola <tarun.bhola@cashlesso.com>
2021-07-17 15:28:07 +0530

    inital commit

--- app/src/main/java/com/learning/cricketfastline/SplashActivity.java
+++ app/src/main/java/com/learning/cricketfastline/SplashActivity.java
@@ -27,7 +27,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                Log.d("hello","hello");
+                Log.d("hello","hellof");
                 startActivity(mainIntent);
                 finish();
             }
